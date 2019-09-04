e02cac8e4d7c19638a3dacc6e5d7ac28fdd525f8
Jacob Huschilt <jhuschilt@connected.io>
2019-09-04 13:24:12 -0400

    Initial commit

---
+++ SwiftUIDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDemo
+//
+//  Created by cl-dev on 2019-09-04.
+//  Copyright © 2019 jacobhuschilt. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
